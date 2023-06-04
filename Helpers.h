88c3274ec873625a1e475979f455303efbe79ef8
grandy35 <uhbif291263@mail.ru>
2023-06-04 18:58:03 +0300

    Initial

---
+++ Helpers.h
@@ -0,0 +1,7 @@
+#include <cmath>
+int test(int a, int b)
+{
+	int sum = a + b;
+	double mul = pow(sum, 2);
+	return mul;
+}
---
+++ Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int result = test(10, 50);
+	std::cout << "result = " << result;
+}
