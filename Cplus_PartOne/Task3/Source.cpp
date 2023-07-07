e89cb499bf339f42939bca25dd5a8515f704798b
grandy35 <uhbif291263@mail.ru>
2023-07-08 00:06:12 +0300

    18.5: added dynamic array and sorting method for this

--- Cplus_PartOne/Task3/Source.cpp
+++ Cplus_PartOne/Task3/Source.cpp
@@ -3,47 +3,66 @@
 
 using namespace std;
 
+
 /*
-	Создайте класс вектора.
-	Его переменные должны быть в приватной области видимости.
-	Показывать значения этих переменных нужно через публичный метод.
-	Дополните класс Vector public методом, который будет возвращать длину (модуль) вектора.
-	на с++
+Создайте класс, полями которого будут имя игрока и количество набранных этим игроком очков.
+Узнайте у пользователя, сколько игроков он хочет добавить, и создайте динамический массив необходимого размера.
+Получите от пользователя имена игроков и набранные ими очки и сохраните в массиве.
+Отсортируйте массив по убыванию количества очков, набранных игроками.
+Выведите все имена и очки игроков в отсортированном виде.
 */
 
-class Vector {
-private:
-	double x;
-	double y;
-	double z;
-
+class Player {
 public:
-	Vector(double x1, double y1, double z1) {
-		x = x1;
-		y = y1;
-		z = z1;
-	}
-
-	double get_x() {
-		return x;
-	}
-
-	double get_y() {
-		return y;
-	}
-
-	double get_z() {
-		return z;
-	}
-
-	int get_lenght() {
-		return pow(x, 2) + pow(y, 2) + pow(z, 2); // x^2 + y^2 + z^2
-	}
+    int score;
+    string name;
 };
 
-int main()
-{
-	Vector vector(5, 10, 15);
-	cout << vector.get_x() << ' ' << vector.get_y() << ' ' << vector.get_z() << endl;
-	cout << "Length of vector = " << vector.get_lenght();
+
+
+void show_players(Player* players, int number_of_players) {
+    cout << endl;
+    for (int i = 0; i < number_of_players; i++) {
+        cout << "Name: " << players[i].name << " ";
+        cout << "Score: " << players[i].score;
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void bubble_sort(Player* players, int number_of_players) {
+    for (int i = 0; i < number_of_players - 1; i++) {
+        for (int j = 0; j < number_of_players - i - 1; j++) {
+            if (players[j].score < players[j + 1].score) {
+                int temp = players[j].score;
+                players[j].score = players[j + 1].score;
+                players[j + 1].score = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    int number_of_players;
+    cout << "how many players do you want: ";
+    cin >> number_of_players;
+
+    Player* players = new Player[number_of_players];
+    for (int i = 0; i < number_of_players; i++) {
+        cout << "Player #" << i + 1 << ". Input name: ";
+        cin >> players[i].name;
+        cout << "Player #" << i + 1 << ". Input score: ";
+        cin >> players[i].score;
+    }
+
+    cout << endl << "Before sorting: " << endl;
+    show_players(players, number_of_players);
+
+    bubble_sort(players, number_of_players);
+    
+    cout << "After sorting: " << endl;
+    show_players(players, number_of_players);
+    delete[number_of_players] players;
 }
